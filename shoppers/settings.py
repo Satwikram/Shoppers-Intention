11febeb965e737db28d5d1ee1a5aeca56825f5f3
Satwik Ram <satwikram29@gmail.com>
2020-10-01 15:21:31 +0530

    Updated

--- shoppers/settings.py
+++ shoppers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2e_)#5cz3s(k_q1whn5b*w&=rr9p^0_zhx*&t&11qrlne&x3+b'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'intention.apps.IntentionConfig',
 ]
 
 MIDDLEWARE = [
